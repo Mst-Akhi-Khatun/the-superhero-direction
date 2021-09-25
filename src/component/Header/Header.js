9862f71c1b49402e1dc4bc3b6f753555ef2bf3f7
Mst-Akhi-Khatun <abiraajmi@gmail.com>
2021-09-26 00:16:42 +0600

    first commit

---
+++ src/component/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="text-center">
+            <h1>Creat A Singer Team</h1>
+        </div>
+    );
+};
+
+export default Header;
