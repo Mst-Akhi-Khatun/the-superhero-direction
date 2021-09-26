b5129a0b852dd2657d3cecace8e6e7e288798105
Mst-Akhi-Khatun <abiraajmi@gmail.com>
2021-09-26 21:35:40 +0600

    all-done

--- src/component/Singer/Singer.js
+++ src/component/Singer/Singer.js
@@ -3,7 +3,7 @@ import './Singer.css';
 
 const Singer = (props) => {
     // destructuring
-    const {name, age, img, born, salary, awards, country} = props.singer;
+    const {name, age, img, born, salary, awards} = props.singer;
     return (
         <div className="col-md-4">
             <div className="card-container">
@@ -15,8 +15,7 @@ const Singer = (props) => {
                     <h6 className="fw-normal">Age:{age}</h6>
                     <h6 className="fw-normal">Salary: {salary}</h6>
                     <h6 className="fw-normal">Born: {born}</h6>
-                    <h6 className="fw-normal">Awards: {awards}</h6>
-                    <h6 className="fw-normal">Country: {country}</h6>
+                    <h6 className="fw-normal">Awards: {awards}</h6>                    
                     <button className="btn w-100 btn-warning" onClick={()=> props.handleAddToCart(props.singer)}> <i class="far fa-check-circle"></i> Select</button>
                 </div>
             </div>
